e596c417bba2b390f4f9f65855a9bf226c1ea9e1
JakubKotulski <jakub.kotulski@interia.com>
2022-04-15 15:34:49 +0200

    successfuly connected with api

---
+++ src/components/RegiserForm/RegisterForm.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Button, Form } from "react-bootstrap";
+import "./RegisterForm.css"
+
+const RegisterForm = () => {
+
+    const colorsForBootstrap = {
+        button: "#007A78"
+    }
+    const [signinUsername, setSigninUsername] = useState("");
+    const [signinPassword, setSigninPassword] = useState("");
+
+    const getUsername = (event) => {
+        setSigninUsername(event.target.value);
+        console.log(signinUsername);
+    }
+
+    const getPassword = (event) => {
+        setSigninPassword(event.target.value);
+        console.log(signinPassword);
+    }
+
+    const signin = () => {
+        axios({
+            method: "POST",
+            data:{
+                username: signinUsername,
+                password: signinPassword,
+            },
+            withCredentials: true,
+            url: "http://localhost:4000/user",
+        }).then((res) => {
+            console.log(res);
+        })
+    }
+
+    return(
+        <div className="register-form">
+            <>
+                <Form className="form">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" onChange={getUsername} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" onChange={getPassword}/>
+                    </Form.Group>
+                    <Button className="button-color" type="submit" onClick={signin}>
+                        Submit
+                    </Button>
+                </Form>
+            </>
+        </div>
+    )
+} 
+
+export default RegisterForm;
