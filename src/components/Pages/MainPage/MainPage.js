5e91c282ba259d818fd473432d67091e7caf731e
JakubKotulski <jakub.kotulski@interia.com>
2022-04-16 09:57:00 +0200

    fetching products, success

--- src/components/Pages/MainPage/MainPage.js
+++ src/components/Pages/MainPage/MainPage.js
@@ -1,6 +1,23 @@
-
+import "./MainPage.css"
+import { useState, useEffect } from "react";
 
 const MainPage = () => {
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const data = await fetch("http://localhost:4000/product");
+
+        const products = await data.json();
+        setProducts(products);
+    }
+
+    console.log(products);
+
     return(
         <h1>MainPage</h1>
     )
