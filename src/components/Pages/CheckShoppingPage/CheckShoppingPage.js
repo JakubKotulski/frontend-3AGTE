b04d68cf666a027e933117227487d83105aae088
JakubKotulski <jakub.kotulski@interia.com>
2022-05-04 13:43:17 +0200

    connection bettwen account balance and price of products bought by user

--- src/components/Pages/CheckShoppingPage/CheckShoppingPage.js
+++ src/components/Pages/CheckShoppingPage/CheckShoppingPage.js
@@ -1,8 +1,8 @@
 import "./CheckShoppingPage.css";
 import CheckList from "../../CheckList/CheckList";
 import AcceptTransactionPanel from "../../AcceptTransactionPanel/AcceptTransactionPanel";
-import { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { useState, useEffect, useCallback } from "react";
+import { Container, Alert, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -11,18 +11,24 @@ const CheckShoppingPage = () => {
     
 
     const navigate = useNavigate();
+    const [list, setList] = useState([]);
+    const [balance, setBalance] = useState(0);
+    const [show, setShow] = useState(false);
+
+    const getBalance = useCallback(() => {
+        axios({
+            method: "GET",
+            withCredentials: true,
+            url: "http://localhost:4000/get-balance",
+        }).then((res) => {
+            setBalance(res.data[0].money);
+        })
+    }, []);
 
     if(localStorage.getItem("array") === null){
         navigate("/");
         window.location.reload(true);
     }
-
-
-    const [list, setList] = useState([]);
-
-    useEffect(() => {
-        getItemsFromStorage();
-    }, [])
 
     const getItemsFromStorage = () => {
         setList(JSON.parse(localStorage.getItem("array")));
@@ -30,7 +36,7 @@ const CheckShoppingPage = () => {
 
     const removeItem = (i) => {
         if(list.length === 1){
-            localStorage.clear();
+            localStorage.removeItem("array");
         }else{
             list.splice(i, 1);
             setList(list);
@@ -47,23 +53,41 @@ const CheckShoppingPage = () => {
         const date = new Date().toLocaleDateString();
 
         if(localStorage.getItem("user")){
-            axios({
-                method: "POST",
-                data: {
-                    items: list,
-                    price: price,
-                    date: date
-                },
-                withCredentials: true,
-                url: "http://localhost:4000/history",
-            }).then((res) => {
-                console.log(res)
-            })
+            if(price <= balance){
+                axios({
+                    method: "POST",
+                    data: {
+                        items: list,
+                        price: price,
+                        date: date
+                    },
+                    withCredentials: true,
+                    url: "http://localhost:4000/history",
+                }).then((res) => {
+                    axios({
+                        method: "PUT",
+                        data: {
+                            balance: balance - price,
+                        },
+                        withCredentials: true,
+                        url: "http://localhost:4000/balance-update",
+                    })
+                    console.log(res)
+                    setList([]);
+                    localStorage.removeItem("array");
+                    navigate("/");
+                })
+            }else{
+                setShow(true);
+            }
+                
         }else{
             console.log("kupione");
+            setList([]);
+            localStorage.removeItem("array");
+            navigate("/");
         }
-        setList([]);
-        localStorage.removeItem("array");
+        
     }
 
     const declineTransaction = () => {
@@ -71,10 +95,23 @@ const CheckShoppingPage = () => {
         localStorage.removeItem("array");
     }
 
+    useEffect(() => {
+        getItemsFromStorage();
+    }, [])
+
+    useEffect(() => {
+        getBalance();
+    }, [])
+
     return(
      <Container>
          <div id = "header-check-list">
-            <h3>Check your list before you finalise the transaction</h3>     
+            <h3>Check your list before you finalise the transaction</h3>   
+            {show && 
+                <Alert id = "bad-alert" variant = "danger">
+                    <p>You don't have enough means in your wallet, just top it up or use traditional bank transfer</p>
+                    <Button variant = "light" onClick = {() => setShow(false)}>Close</Button>
+                </Alert>}  
         </div>
         <div>
             <CheckList list = { list } removeFunction = {removeItem} fetch = {getItemsFromStorage}/>
@@ -82,7 +119,7 @@ const CheckShoppingPage = () => {
         <div>
             <AcceptTransactionPanel accept = {acceptTransaction} decline = {declineTransaction} />
         </div>
-
+        
      </Container>
     )
 }
